2a6716a5144fd3bb788957ec0c56a7cd4cc51d53
Nitin <nkmehra77@gmail.com>
2023-03-16 13:17:58 +0530

    designed login page

--- src/components/UI/Dashboard.js
+++ src/components/UI/Dashboard.js
@@ -1,17 +1,25 @@
 import { Box } from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
+import { useSelector } from "react-redux";
+import Login from "../LogIn";
 import Rightbar from "./Rightbar";
 import SideBar from "./SideBar";
 
 function Dashboard() {
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
   return (
-    <Box>
-      <Stack direction='row' spacing={2} justifyContent='space-between'>
-        <SideBar />
-        <Rightbar />
-      </Stack>
-    </Box>
+    <>
+      {!isLoggedIn && <Login />}
+      {isLoggedIn && (
+        <Box>
+          <Stack direction='row' spacing={2} justifyContent='space-between'>
+            <SideBar />
+            <Rightbar />
+          </Stack>
+        </Box>
+      )}
+    </>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import LoginSlice from "./LoginSlice";
 import usersListSlice from "./UserListSlice";
 
 const store = configureStore({
-  reducer: { userList: usersListSlice },
+  reducer: { userList: usersListSlice, login: LoginSlice },
 });
 export default store;
