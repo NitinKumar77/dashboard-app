398b8d288907da01109049ba74db36b6dcec2a7a
Nitin <nkmehra77@gmail.com>
2023-03-17 16:01:19 +0530

    changin color implemented

--- src/components/UI/SideBar.js
+++ src/components/UI/SideBar.js
@@ -12,10 +12,12 @@ import ViewInArTwoToneIcon from "@mui/icons-material/ViewInArTwoTone";
 import React from "react";
 import { setLoggedIn, setLoginError } from "../../redux/LoginSlice";
 import { useDispatch } from "react-redux";
-import { NavLink, redirect } from "react-router-dom";
+import { NavLink, redirect, useLocation } from "react-router-dom";
 
 function SideBar() {
   const dispatch = useDispatch();
+  const location = useLocation();
+
   return (
     <Box
       flex={1}
@@ -28,21 +30,31 @@ function SideBar() {
       <Box position={"fixed"}>
         <List>
           <ListItem disablePadding>
-            <ListItemButton component={NavLink} to='/'>
+            <ListItemButton
+              component={NavLink}
+              to='/'
+              sx={{
+                color: location.pathname === "/" ? "#61FF48" : undefined,
+              }}
+            >
               <ListItemIcon>
-                <AccountCircleIcon fontSize='large' sx={{ color: "#61FF48" }} />
+                <AccountCircleIcon
+                  fontSize='large'
+                  sx={{
+                    color: location.pathname === "/" ? "#61FF48" : undefined,
+                  }}
+                />
               </ListItemIcon>
               <Typography
                 primary='Users'
                 sx={{
                   fontFamily: "Inter",
-
                   fontStyle: "normal",
                   fontWeight: "700",
                   fontSize: "30px",
                   lineHeight: "39px",
                   /* identical to box height */
-                  color: "#61FF48",
+                  color: location.pathname === "/" ? "#61FF48" : undefined,
                 }}
               >
                 Users
@@ -50,20 +62,33 @@ function SideBar() {
             </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton component={NavLink} to='/products'>
+            <ListItemButton
+              component={NavLink}
+              to='/products'
+              sx={{
+                color:
+                  location.pathname === "/products" ? "#61FF48" : undefined,
+              }}
+            >
               <ListItemIcon>
-                <ViewInArTwoToneIcon fontSize='large' />
+                <ViewInArTwoToneIcon
+                  fontSize='large'
+                  sx={{
+                    color:
+                      location.pathname === "/products" ? "#61FF48" : undefined,
+                  }}
+                />
               </ListItemIcon>
               <Typography
                 primary='Products'
                 sx={{
                   fontFamily: "Inter",
-
                   fontStyle: "normal",
                   fontWeight: "400",
                   fontSize: "28px",
                   lineHeight: "39px",
-                  color: "#777575",
+                  color:
+                    location.pathname === "/products" ? "#61FF48" : "#777575",
                 }}
               >
                 Products
@@ -85,7 +110,6 @@ function SideBar() {
               <Typography
                 sx={{
                   fontFamily: "Inter",
-
                   fontStyle: "normal",
                   fontWeight: "400",
                   fontSize: "28px",
